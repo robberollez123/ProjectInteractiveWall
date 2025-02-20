39375b9855f76818b9b3cc2018f5d3b926ad835b
robberollez123 <robbe.rollez@telenet.be>
2025-02-20 16:53:31 +0100

    De html taal veranderen naar Engels

--- Project/Code/Website/NAV PAGES/PHP/games.php
+++ Project/Code/Website/NAV PAGES/PHP/games.php
@@ -28,7 +28,7 @@ if ($adminUser && isset($_POST["submit"])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="nl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- Project/Code/Website/NAV PAGES/PHP/home.php
+++ Project/Code/Website/NAV PAGES/PHP/home.php
@@ -50,7 +50,7 @@ $welkomMessage = $ingelogd ? "Welkom, " . $_SESSION["user"] . "!" : "Welkom bij 
 ?>
 
 <!DOCTYPE html>
-<html lang="nl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
